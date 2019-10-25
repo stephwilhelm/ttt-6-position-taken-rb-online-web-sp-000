c4a545555a205c136effadf0363b51bc583e4bf9
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-25 05:11:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,11 @@
 # code your #position_taken? method here!
-def board
-  board = [" "," "," "," "," "," "," "," "," "] || board = [""," "," "," "," "," "," "," "," "] || board = ["","","","","","","","",""]
-end
-
-def index
-  index = [" "," "," "," "," "," "," "," "," "] || index = [""," "," "," "," "," "," "," "," "] || index = ["","","","","","","","",""] || index = ["nil","","","","","","","",""]
-end
-
 def position_taken?(board, index)
-  if expect(position_taken(index)).to be(true) == "O" || (position_taken(board, index)).to be(true) ==  "X"
 
-  elsif expect(position_taken(index)).to be(false) == " " || (position_taken(board, index)).to be(false) == "" || (position_taken(board, index)).to be(false) == "nil".
-
+  taken = nil
+  if (board[index] == " " || board[index] == "" || board[index] == "nil")
+    taken = false
+  else
+    taken = true
   end
+  taken
 end
