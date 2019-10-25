cdf8da4d1476dd2b546b16e9349234cbe2a74a6e
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-25 05:14:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
 
   taken = nil
-  if (board[index] == " " || board[index] == "" || board[index] == "nil")
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
